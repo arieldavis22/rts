e62c858b1d4160226fdb3375d80dd61a1caeb7c0
Ariel Davis <amanichai100@gmail.com>
2022-09-06 13:05:49 -0400

    drag handler

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -5,9 +5,17 @@ const EventComponent: React.FC = () => {
         
     }
 
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+       console.log(event);
+        
+    }
+
     return (
         <div>
             <input onChange={onChange} />
+            <div draggable onDragStart={onDragStart}>
+                Drag Me!
+            </div>
         </div>
     )
 }
